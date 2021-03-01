08c516af8875431e6d43dcb49996108cd8fcd40e
samehmasri <sameh.s.almasri@gmail.com>
2021-03-01 22:11:36 +0200

    lab12

--- server.js
+++ server.js
@@ -4,62 +4,97 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 const superagent = require('superagent');
+const pg = require('pg');
 
+// const client = new pg.Client(process.env.DATABASE_URL)
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
-// Routes Definitions
 app.set('view engine', 'ejs');
-app.use(express.urlencoded());//to use dot body
-// to access the public and use style 
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'))
-app.get('/', homeHandler);
-app.post('/searches/show', showHandler);
 
 
+app.get('/getBook/:bookID', bookDetailsHandler)
+app.get('/searches/new', newBookHandler)
+app.post('/addBook', addBookHandler)
+app.get('/', (req, res) => {
+    let SQL = `SELECT * FROM books ORDER BY id DESC;`;
+    client.query(SQL)//access the db
+        .then((results) => {
+            // console.log(results.rows);
+            res.render('pages/books/show', { booksArr: results.rows })//render data saved from db
+        })
+});
 
-// Routes handler
-function homeHandler(req, res) {
-    res.render('pages/searches/new');
-};
-
-function showHandler(req, res) {
-    let searchData = req.body.searchBox;// access new and take the searchbox
-    let authorOrTitle = Object.keys(req.body);
-    // console.log((req.body));
-    //https://www.googleapis.com/books/v1/volumes?q=(amman)+in(title)
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${searchData}+in${authorOrTitle[1]}`
+app.post('/searches/show', (req, res) => {
+    let searchData = req.body.searchBox;
+    let authorOrTitle = req.body.titleAuthor;
+    console.log(req.body);
+    let url = `https://www.googleapis.com/books/v1/volumes?q=+in${authorOrTitle}:${searchData}`
+    console.log(url);
     superagent.get(url)
         .then((results) => {
-
-            // map to apply function on each element in array
             let bookData = results.body.items.map(book => {
                 return new Book(book)
+
             })
             res.render('pages/searches/show', { data: bookData });
         })
         .catch(() => {
             errorHandler('Cannot Catch your Data from API', req, res)
+
         })
+})
+
+
+
+function Book(data) {
+    this.author = data.volumeInfo.authors || `There is no Authors`;
+    this.title = data.volumeInfo.title;
+    // this.isbn = data.volumeInfo.industryIdentifiers[0].identifier || 'there is no isbn';
+    this.isbn = data.volumeInfo.industryIdentifiers ? data.volumeInfo.industryIdentifiers[0].identifier || 'There is no isbn found' : 'There is no isbn found';
+    this.image_url = data.volumeInfo.imageLinks.thumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
+    this.description = data.volumeInfo.description || `There is no description`
+
 }
 
 function errorHandler(error, req, res) {
     res.render('pages/error', { err: error });
 
 };
+client.connect()//connect db
+    .then(() => {
+        app.listen(PORT, () => {//connect port
+            console.log(`listening on port ${PORT}`);
+        });
+    });
 
-
-// Constructor
-function Book(data) {
-    this.title = data.volumeInfo.title;
-    this.image = data.volumeInfo.imageLinks.thumbnail || `https://i.imgur.com/J5LVHEL.jpg`;
-    this.author = data.volumeInfo.authors || `There is no Authors`;
-    this.description = data.volumeInfo.description || `There is no description`
+function addBookHandler(req, res) {
+    console.log(req.body);
+    let { author, title, isbn, image_url, description } = req.body;
+    let SQL = `INSERT INTO books (author, title, isbn, image_url, description) VALUES ($1,$2,$3,$4,$5);`;
+    let safeValues = [author, title, isbn, image_url, description];
+    client.query(SQL, safeValues)
+        .then(() => {
+            res.redirect('/');
+        })
 
 }
 
+function newBookHandler(req, res) {
+    res.render('pages/searches/new');
+}
 
+function bookDetailsHandler(req, res) {
+    let SQL = `SELECT * FROM books WHERE id=$1`;
+    let book_id = req.params.bookID;
+    let values = [book_id];
+    client.query(SQL, values)
+        .then(results => {
+            res.render('pages/books/detail', { bookDetails: results.rows[0] });
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-});
+        })
+}
